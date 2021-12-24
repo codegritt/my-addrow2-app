d63f7f98f030bc40abfdb390209238714292739c
Gokul <gokulchandan18@gmail.com>
2021-12-24 16:19:11 +0530

    commit

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import{HttpClient} from '@angular/common/http'
+import { map } from 'rxjs/operators';
+import { identifierName } from '@angular/compiler';
 
 
 @Injectable({
@@ -11,6 +13,31 @@ export class ApiService {
 
   postEmployee(data: any){
     return this.http.post<any>("http://localhost:3000/employees", data)
-   
+    .pipe(map((res:any)=>{
+      return res;
+    }))
   }
+
+  getEmployee(){
+    return this.http.get<any>("http://localhost:3000/employees")
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+
+  updateEmployee(data: any,id: number){
+    return this.http.put<any>("http://localhost:3000/employees/"+id,data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+
+  deleteEmployee(id: number){
+    return this.http.delete<any>("http://localhost:3000/employees/"+id)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+
+
 }
