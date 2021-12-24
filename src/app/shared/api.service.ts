32ea3c108d3e0b6c79d8cbade284cae4466ab840
Gokul <gokulchandan18@gmail.com>
2021-12-24 16:06:34 +0530

    commit

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import{HttpClient} from '@angular/common/http'
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postEmployee(data: any){
+    return this.http.post<any>("http://localhost:3000/employees", data)
+    .pipe(map())
+  }
 }
