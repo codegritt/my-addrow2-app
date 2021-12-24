51a39b23039b07c99393c2a6410ccdcba795dab8
Gokul <gokulchandan18@gmail.com>
2021-12-24 16:32:53 +0530

    commit

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { EmployeeModel } from './employee-dash board.model';
 
 @Component({
@@ -10,25 +10,35 @@ import { EmployeeModel } from './employee-dash board.model';
 export class EmployeeDashboardComponent implements OnInit {
 
   formValue !: FormGroup;
-  employeeModelObj : EmployeeModel = new EmployeeModel();
+  employeeModelObj: EmployeeModel = new EmployeeModel();
+  api: any;
 
   constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit(): void {
-this.formValue=this.formbuilder.group({
-  Id:[''],
-  first_name:[''],
-  last_name:[''],
-  email:['']
-})
+    this.formValue = this.formbuilder.group({
+      Id: [''],
+      first_name: [''],
+      last_name: [''],
+      email: ['']
+    })
 
   }
 
-  postEmployeeDetails(){
+  postEmployeeDetails() {
     this.employeeModelObj.id = this.formValue.value.id;
     this.employeeModelObj.firstName = this.formValue.value.firstName;
     this.employeeModelObj.lastName = this.formValue.value.lastName;
     this.employeeModelObj.email = this.formValue.value.email;
+
+    this.api.postEmployee(this.employeeModelObj)
+      .subscribe((res: any) => {
+        console.log(res);
+        alert("Employee added successfully")
+      },
+        (err: any) => {
+          alert("Something went wrong")
+        })
 
   }
 
